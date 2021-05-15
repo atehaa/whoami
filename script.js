5facd4f73ad3cca42b2926662400efce7bdd244e
Murat Süzük <muratsuzuk@gmail.com>
2021-05-15 16:59:28 +0300

    Initial Commit
    Initial Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const carouselText = [
+  {text: "Software Engineer", color: "red"},
+  {text: "RPA Developer", color: "orange"},
+  {text: "Tech Geek!", color: "yellow"}
+]
+
+$( document ).ready(async function() {
+  carousel(carouselText, "#feature-text")
+});
+
+async function typeSentence(sentence, eleRef, delay = 70) {
+  const letters = sentence.split("");
+  let i = 0;
+  while(i < letters.length) {
+    await waitForMs(delay);
+    $(eleRef).append(letters[i]);
+    i++
+  }
+  return;
+}
+
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+  let i = 0;
+  while(letters.length > 0) {
+    await waitForMs(50);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
+  }
+}
+
+async function carousel(carouselList, eleRef) {
+    var i = 0;
+    while(true) {
+      updateFontColor(eleRef, carouselList[i].color)
+      await typeSentence(carouselList[i].text, eleRef);
+      await waitForMs(1000);
+      await deleteSentence(eleRef);
+      await waitForMs(300);
+      i++
+      if(i >= carouselList.length) {i = 0;}
+    }
+}
+
+function updateFontColor(eleRef, color) {
+  $(eleRef).css('color', color);
+}
+
+function waitForMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
